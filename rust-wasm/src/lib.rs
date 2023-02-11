97d7b4332a26705eb66bb3d14bd6f75924aa8529
Arpit Jain <arpitjp@gmail.com>
2023-02-11 13:20:02 +0530

    init, memory working

--- rust-wasm/src/lib.rs
+++ rust-wasm/src/lib.rs
@@ -14,6 +14,6 @@ extern {
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, rust-wasm!");
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}", name));
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './index.css';
+import init, { greet } from 'rust-wasm';
+
+const raw = await init();
 
 function App() {
   return (
     <div id="playground">
       <h2>React Playground</h2>
+      <button onClick={() => greet("World!")}>
+        Click Me
+      </button>
     </div>
   )
 }
