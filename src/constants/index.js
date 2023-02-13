18b73116fb860288ddf1484f7501926a6265d8be
Arpit Jain <arpitjp@gmail.com>
2023-02-13 14:17:57 +0530

    canvas css

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
 export const CELL_SIZE = 15; // px
-export const GRID_COLOR = "rgb(0, 0, 0, 0.07)";
+export const GRID_COLOR = "rgb(0, 0, 0, 0.1)";
 export const DEAD_COLOR = "#FFFFFF";
 export const ALIVE_COLOR = "#000000";
