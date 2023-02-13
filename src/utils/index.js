a75a21adcf0d87c3438612f7201e0fb9bb44becb
Arpit Jain <arpitjp@gmail.com>
2023-02-13 14:03:48 +0530

    don't draw borders

--- src/utils/index.js
+++ src/utils/index.js
@@ -9,13 +9,13 @@ export const drawGrid = ({ ctx, width, height }) => {
   ctx.strokeStyle = GRID_COLOR;
 
   // Vertical lines.
-  for (let i = 0; i <= width; i++) {
+  for (let i = 1; i < width; i++) {
     ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
     ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
   }
 
   // Horizontal lines.
-  for (let j = 0; j <= height; j++) {
+  for (let j = 1; j < height; j++) {
     ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
     ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
   }
