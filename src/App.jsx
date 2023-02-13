a173ae602b680c8f337622e8609448c6178ef3c8
Arpit Jain <arpitjp@gmail.com>
2023-02-13 11:41:29 +0530

    metadata changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import image from "./assets/github-mark.png";
 import { useUniverse } from "./hooks/useUniverse";
 import { Canvas } from "./components/canvas.jsx";
 
+document.head.appendChild(Object.assign(document.createElement("link"), {rel: "icon", href: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🧩</text></svg>"}))
+
 function App() {
   const { universe, h, w } = useUniverse();
   const [isPlaying, setIsPlaying] = useState(true);
